f8aaa0876877d600e0862c7b515e3136928c8540
Richard Udeh <richardudeh03@gmail.com>
2023-09-11 01:15:16 +0100

    third commit

--- script.js
+++ script.js
@@ -18,23 +18,32 @@ const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Sa
 
 const dayWeek = new Date();
 const DayOfTheWeek = weekday[dayWeek.getDay()];
-// to get the current UTC 
+// to get the current UTC
+
+const showTime =()=> {
 const now = new Date();
 
-const year = now.getUTCFullYear();
-const month = now.getUTCMonth() + 1;
-const day = now.getUTCDate();
-const hours = now.getUTCHours() + 1;
-const minutes = now.getUTCMinutes();
-const seconds = now.getUTCSeconds();
-const milliseconds = now.getUTCMilliseconds();
+let utcHours = now.getUTCHours() + 1;
+let utcMinutes = now.getUTCMinutes();
+let utcSeconds = now.getUTCSeconds();
+let utcTime;
 
+utcHours = utcHours < 10 ? `0${utcHours} ` : utcHours;
+utcMinutes = utcMinutes < 10 ? `0${utcMinutes}` : utcMinutes;
+utcSeconds = utcSeconds < 10 ? `0${utcSeconds}` : utcSeconds;
 
+utcTime = ` Utc Time : ${utcHours}:${utcMinutes}:${utcSeconds}`;
+CurrentUTCTime.textContent= utcTime;
+
+// function for UTC time update
+setTimeout(showTime,1000);
+};
+showTime();
 
 // DOM manipulation
 slackName.innerText=`Name: Richard Udeh`
 slackProfileImage.innerHTML = imgElement.outerHTML;
 CurrentDayOfTheWeek .innerText = `Day of the week : ${DayOfTheWeek} `;
-CurrentUTCTime.innerHTML=`Time in UTC : ${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${milliseconds}`;
+
 track.innerText = `Track:Frontend`;
 GithubURL.innerHTML = `<a href="https://github.com/Richardudeh4/slack-Intern-profile-card"> GitHub Url</a>`
